367a4e58e82ce4a179094ae901ec5a8154780fda
notquirrel <mudaf127@gmail.com>
2023-07-10 08:57:25 +0700

    Home & Layout changes

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -245,7 +245,7 @@
                             <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
                         </a>
                         <ul aria-expanded="false">
-                            <li><a href="./index.html">Home 1</a></li>
+                            <li><a href="/home">Home</a></li>
                             <!-- <li><a href="./index-2.html">Home 2</a></li> -->
                         </ul>
                     </li>
