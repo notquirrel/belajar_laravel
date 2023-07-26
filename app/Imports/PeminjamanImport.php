a46a029912f0b7951d1cb7865aa6c3eb74e55fc4
notquirrel <mudaf127@gmail.com>
2023-07-26 10:04:41 +0700

    Fixed Excel Import for Table Relation

--- app/Imports/PeminjamanImport.php
+++ app/Imports/PeminjamanImport.php
@@ -2,24 +2,33 @@
 
 namespace App\Imports;
 
+use App\Models\Buku;
 use App\Models\Peminjaman;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class PeminjamanImport implements ToModel, WithHeadingRow
+class PeminjamanImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new Peminjaman([
-            'nama'         => $row['nama'],
-            'id_buku'      => $row['judul_buku'],
-            'tgl_pinjam'   => date($row['tgl_pinjam']),
-            'tgl_kembali'  => date($row['tgl_kembali']),
-        ]);
+        foreach($rows as $row){
+            $buku = Buku::where('judul_buku', $row['judul_buku'])->first();
+
+            if($buku != null){
+                Peminjaman::create([
+                    'nama'         => $row['nama'],
+                    'id_buku'      => $buku['id'],
+                    'tgl_pinjam'   => date($row['tgl_pinjam']),
+                    'tgl_kembali'  => date($row['tgl_kembali']),
+                ]);
+            }
+        }
     }
 }
